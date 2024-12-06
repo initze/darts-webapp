38509d4aaf1ab4afe9a7cf0bcc7a0edafed4d667
Ingmar Nitze <ingmarnitze@gmail.com>
2024-12-06 18:10:29 +0100

    updated impoert issue

--- darts_streamlit_app.py
+++ darts_streamlit_app.py
@@ -1,16 +1,29 @@
 import branca
 import geopandas as gpd
-import leafmap
+import leafmap.foliumap as leafmap
 import streamlit as st
 from streamlit_folium import st_folium
 
 # load data
-def create_map(gdf, gdf_footprints):
-    m = leafmap.Map(zoom=9)
-    # Add ESRI World Imagery basemap
-    # m.add_basemap("Esri.WorldImagery")
+# fpath = "data/DARTS_NitzeEtAl_v1_features_2018_2023_level2.parquet"
+fpath = 'https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A91b12042-18fb-4ded-8854-124ff3be6a11'
+# gdf = gpd.read_file(fpath)
+# gdf = gpd.read_parquet(fpath)
+fpath_union = "DARTS_NitzeEtAl_v1_coverage_2018_2023_union.parquet"
+gdf_footprints = gpd.read_parquet(fpath_union)
 
-    m.add_gdf(gdf_footprints, layer_name="DARTS maximum extent", style_function=lambda x: style_footprints)
+style_footprints = {
+    "fillColor": "green",
+    "color": "white",  # Default color for lines (for year 2018)
+    "weight": 1,
+    "fillOpacity": 0.1,
+}
+
+# load data
+def create_map():
+    m = leafmap.Map(zoom=5)
+    # Add ESRI World Imagery basemap
+    m.add_basemap("Esri.WorldImagery")
     # m.add_gdf(gdf[:20000], layer_name="DARTS level 2", zoom_to_layer=True, style_function=style_function)
     return m
 
@@ -34,25 +47,16 @@ def style_function(feature):
 
     return style
 
-
-style_footprints = {
-    "fillColor": "green",
-    "color": "white",  # Default color for lines (for year 2018)
-    "weight": 1,
-    "fillOpacity": 0.1,
-}
-
-
-# load data
-# fpath = "data/DARTS_NitzeEtAl_v1_features_2018_2023_level2.parquet"
-fpath_union = "DARTS_NitzeEtAl_v1_coverage_2018_2023_union.parquet"
-# gdf = gpd.read_parquet(fpath)
-gdf_footprints = gpd.read_parquet(fpath_union)
-
 # Streamlit app layout
 st.title("DARTS")
-# m = leafmap.Map(zoom=9)
-m = create_map(None, gdf_footprints)
 
-folium_map = m.to_folium()
-st_folium(folium_map, width='100%', returned_objects=['last_object_clicked'])
+# Streamlit app layout
+st.title("Dynamic Polygon Dashboard")
+
+# Create the map with the GeoDataFrame layer
+m = create_map(gdf_footprints)
+m.add_gdf(gdf_footprints, layer_name="DARTS maximum extent", style_function=lambda x: style_footprints)
+# m.add_gdf(gdf, layer_name='DARTS v1 level2', style_function=style_function)
+
+# Display the map in Streamlit using st_folium and capture clicked features
+clicked_feature = st_folium(m, width='100%', returned_objects=['last_object_clicked'])
